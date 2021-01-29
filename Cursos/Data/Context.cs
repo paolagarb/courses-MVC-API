23622b1d56ef24419009175f806c5d6b667fa727
Paola Garbato <paolagarbato@gmail.com>
2021-01-29 14:10:40 -0300

    Adicionando conexão com bd, first migration

---
+++ Cursos/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Cursos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cursos.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Plataforma> Plataformas { get; set; }
+    }
+}
---
+++ Cursos/Migrations/20210129170945_Initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cursos.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plataformas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plataformas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlataformaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cursos_Plataformas_PlataformaId",
+                        column: x => x.PlataformaId,
+                        principalTable: "Plataformas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_PlataformaId",
+                table: "Cursos",
+                column: "PlataformaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "Plataformas");
+        }
+    }
+}
