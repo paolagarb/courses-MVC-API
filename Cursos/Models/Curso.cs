e985a88092204b968e2aaa4f20656706577029eb
Paola Garbato <paolagarbato@gmail.com>
2021-01-29 14:03:44 -0300

    Adicionando classes

---
+++ Cursos/Models/Curso.cs
@@ -0,0 +1,14 @@
+﻿namespace Cursos.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Plataforma Plataforma { get; set; }
+        public int PlataformaId { get; set; }
+        public Curso()
+        {
+
+        }
+    }
+}
---
+++ Cursos/Models/Plataforma.cs
@@ -0,0 +1,12 @@
+﻿namespace Cursos.Models
+{
+    public class Plataforma
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Plataforma()
+        {
+                
+        }
+    }
+}
